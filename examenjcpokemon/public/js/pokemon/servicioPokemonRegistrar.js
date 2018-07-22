6c5ccdb4d1e3f835d292ed79f1fa4236e7c86db3
tomataso <tomataso@users.noreply.github.com>
2018-07-21 23:28:16 -0600

    html link change

--- examenjcpokemon/public/js/pokemon/servicioPokemonRegistrar.js
+++ examenjcpokemon/public/js/pokemon/servicioPokemonRegistrar.js
@@ -45,32 +45,3 @@ function registrarPokemon(pPokemon) {
 
 
 
-function obtenerListaTipo(){
-    let listaTipo = [];
-
-    let respuesta = '';
-    let peticion = $.ajax({
-        url : 'http://localhost:4000/api/listartipo',
-        type : 'get',
-        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
-        dataType : 'json',
-        async : false,
-        data:{
-            
-        }
-      });
-    
-      peticion.done(function(response){
-       respuesta = response;
-      });
-    
-      peticion.fail(function(response){
-       
-      });
-
-      return respuesta;
-    
-    return listaTipo;
-}
-
-
