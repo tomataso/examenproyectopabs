4af90518f11f14520ff273b09c0b16ef2f7f7c84
tomataso <tomataso@users.noreply.github.com>
2018-07-22 12:49:11 -0600

    Cambio esquema entrenador 2

--- examenjcpokemon/public/js/entrenador/servicioEntrenadorRegistrar.js
+++ examenjcpokemon/public/js/entrenador/servicioEntrenadorRegistrar.js
@@ -38,32 +38,3 @@ function registrarEntrenador(nCodigoEntrenador, sNombreEntrenador, nEdad, select
 }
 
 
-
-
-function obtenerListaEntrenador(){
-    let listaEntrenador = [];
-
-    let respuesta = '';
-    let peticion = $.ajax({
-        url : 'http://localhost:4000/api/listarEntrenador',
-        type : 'get',
-        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
-        dataType : 'json',
-        async : false,
-        data:{
-            
-        }
-      });
-    
-      peticion.done(function(response){
-       respuesta = response;
-      });
-    
-      peticion.fail(function(response){
-       
-      });
-
-      return respuesta;
-    
-    return listaEntrenador;
-}
